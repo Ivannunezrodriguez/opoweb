894d5ae6ad570d23f3c2da3d73b08f4efcd3e90c
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2026-07-10 15:03:10 +0200

    Actualizar caché PWA a v55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'opoweb-v54';
+const CACHE = 'opoweb-v55';
 const ASSETS = [
   './',
   './index.html',
@@ -37,12 +37,14 @@ const ASSETS = [
   './assets/js/diputacion-v45.js',
   './assets/js/diputacion-v46.js',
   './assets/js/diputacion-v47.js',
+  './assets/js/diputacion-v48.js',
   './assets/js/ui-v42.js',
   './assets/js/ui-v43.js',
   './assets/js/ui-v44.js',
   './assets/js/ui-v45.js',
   './assets/js/ui-v46.js',
   './assets/js/ui-v47.js',
+  './assets/js/ui-v48.js',
   './data/oposiciones.js',
   './data/proceso.js',
   './data/uc3m.js',
