9f08bb724f05b326cf433e67c28c26931b62b441
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2026-07-15 19:37:05 +0200

    Incorporar hardware y periféricos de Diputación a la PWA

--- assets/js/asset-manifest-v83.js
+++ assets/js/asset-manifest-v83.js
@@ -151,6 +151,7 @@
     './assets/js/diputacion-teoria-v87-bloque3.js',
     './assets/js/diputacion-test-v87-bloque3.js',
     './assets/js/diputacion-teoria-v87-bloque4.js',
+    './assets/js/diputacion-teoria-v87-bloque5.js',
     './assets/js/ui-v87.js'
   ];
 
